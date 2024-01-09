9636c3d8f44f8b70a3a23ec77175230539b17d17
Don Barto Jr <donbartojunior@gmail.com>
2024-01-09 08:15:45 -0500

    remove random show again

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import logo from './NewStarlake2.PNG';
 import './App.css';
 import Countdown from './modules/Countdown';
-import RandomShow from './modules/RandomShow';
 
 function App() {
   return (
@@ -13,7 +12,7 @@ function App() {
       </h1>
       <img src={logo} className="App-logo" alt="logo" />
       <Countdown />
-      {/* <RandomShow /> */}
+      
       
       </header>
       
